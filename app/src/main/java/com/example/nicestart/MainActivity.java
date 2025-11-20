27b6be016b00ea233993fd27eb5f34212dc32bba
Guillermo-Leal <guillermo.lealjimenez1@gmail.com>
2025-11-20 09:51:33 +0100

    added menu_appbar and menu_context

--- app/src/main/java/com/example/nicestart/MainActivity.java
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -2,30 +2,83 @@ package com.example.nicestart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView mycontext = findViewById(R.id.mytext);
+        registerForContextMenu(mycontext);
+
+        swipeLayout = findViewById(R.id.swipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
     }
 
-    public void openLogin(View v){
-        Intent intent = new Intent(MainActivity.this, Login.class);
-        startActivity(intent);
+    //implementar menu contextual
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
     }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+//                item.getMenuInfo();
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item copied",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this, "Downloading item...",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem menu){
+        int id = item.getItemId();
+
+        if(id==R.id.Settings){
+            Toast toast = Toast.makeText(this, "Infecting", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }*/
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(MainActivity.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
+           toast0.show();
+           swipeLayout.setRefreshing(false);
+        }
+    };
+
 }
